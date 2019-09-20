5a41d90e435914a71d71e674050217995164b62d
Warren Yao <warrenyca905@gmail.com>
2019-09-20 02:45:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+  
+  get '/' do
+    @session = session
+    erb :index
+  end
+  
+  post '/checkout' do
+    @item = params[:item]
+    session[:item] = @item
+    erb :display
+  end
 end
